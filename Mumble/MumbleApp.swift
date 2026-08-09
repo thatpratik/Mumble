d0864a5e7b949ea2639c2ccdd69f82b1a3fb597e
Pratik Sharma <pratiksharma@live.in>
2026-08-09 21:57:15 +0200

    Initial Xcode project scaffold

---
+++ Mumble/MumbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MumbleApp.swift
+//  Mumble
+//
+//  Created by Pratik Sharma on 09/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct MumbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
